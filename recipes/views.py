6e09e472f93703da27c3b370c9e34e7fa6a635fc
João NOT Pessoal <>
2023-12-18 20:52:58 -0300

    template e css inicial da pagina da lista de receitas

--- recipes/views.py
+++ recipes/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request,'recipes/home.html')
+    return render(request,'recipes/pages/home.html')
