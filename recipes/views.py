f02f7448419eaa18cfded734d8857be6197ee2db
João Paulo <joaopaulosilva39.jp@gmail.com>
2023-12-19 11:22:36 -0300

    Heranças, Blocks

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request,'recipes/pages/home.html')
+
+def recipe(request, id):
+    return render(request,'recipes/pages/recipe-view.html')
