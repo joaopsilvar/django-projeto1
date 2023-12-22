4d02ff743390a3cafa8db9af38774719373c4b57
João NOT Pessoal <>
2023-12-22 07:43:43 -0300

    Update Models

--- recipes/views.py
+++ recipes/views.py
@@ -2,24 +2,42 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from utils.recipes.factory import make_recipe
 from .models import Recipe,Category
+from django.http import Http404
 
 # Create your views here.
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request,'recipes/pages/home.html',context={
         'recipes': recipes,
     })
 
 def category(request,category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
-    return render(request,'recipes/pages/home.html',context={
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True).order_by('-id')
+    
+    if not recipes:
+        raise Http404('Not Found')
+
+
+    return render(request,'recipes/pages/category.html',context={
         'recipes': recipes,
+        'title' : f'{recipes.first().category.name} - Category | '
     })
 
 def recipe(request, id):
-    recipe = Recipe.objects.get(id=id)
+    recipe = Recipe.objects.get(
+        id=id,
+        is_published=True
+    )
     return render(request,'recipes/pages/recipe-view.html',context={
         'recipe': recipe,
         'is_detail_page': True,
+    })
+
+def search(request,search_text):
+    recipes = Recipe.objects.filter(title=search_text)
+    return render(request,'recipes/pages/home.html',context={
+        'recipes': recipes,
     })
