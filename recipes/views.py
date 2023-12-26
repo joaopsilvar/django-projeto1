e2bf4b7db5531c81276764e6867cf4d484a951bf
João Paulo <joaopaulosilva39.jp@gmail.com>
2023-12-26 16:06:47 -0300

    test dois

--- recipes/views.py
+++ recipes/views.py
@@ -25,7 +25,7 @@ def category(request,category_id):
     })
 
 def recipe(request, id):
-    recipe = get_object_or_404(Recipe,is_published=True)
+    recipe = get_object_or_404(Recipe,id=id,is_published=True)
 
     return render(request,'recipes/pages/recipe-view.html',context={
         'recipe': recipe,
