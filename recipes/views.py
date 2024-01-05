6265858e8ee27088dbd906f05619ffd94a5cf28a
João Paulo <joaopaulosilva39.jp@gmail.com>
2024-01-05 11:25:39 -0300

    seperação dos testes da view

--- recipes/views.py
+++ recipes/views.py
@@ -1,33 +1,38 @@
-from django.shortcuts import render,get_list_or_404, get_object_or_404
-from django.http import HttpResponse, Http404
-from utils.recipes.factory import make_recipe
-from .models import Recipe,Category
+from django.db.models import Q
+from django.http.response import Http404
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
-# Create your views here.
+from recipes.models import Recipe
+
 
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
-    
-    return render(request,'recipes/pages/home.html',context={
+    recipes = Recipe.objects.filter(
+        is_published=True,
+    ).order_by('-id')
+
+    return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
-def category(request,category_id):
+
+def category(request, category_id):
     recipes = get_list_or_404(
         Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True).order_by('-id')
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
     )
 
-    return render(request,'recipes/pages/category.html',context={
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title' : f'{recipes[0].category.name} - Category | '
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
-def recipe(request, id):
-    recipe = get_object_or_404(Recipe,id=id,is_published=True)
 
-    return render(request,'recipes/pages/recipe-view.html',context={
+def recipe(request, id):
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
+
+    return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
     })
@@ -36,7 +41,17 @@ def search(request):
     search_term = request.GET.get('q', '').strip()
     if not search_term:
         raise Http404()
-    return render(request,'recipes/pages/search.html', context={
-        'page_title' : f'Search for "{search_term}" | ',
-        'search_term' : search_term,
+
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
+
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}" |',
+        'search_term': search_term,
+        'recipes': recipes,
     })
