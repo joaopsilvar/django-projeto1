3311c18483e6a8e0e48feb35a8f46f5323d5e9cf
João Paulo <joaopaulosilva39.jp@gmail.com>
2024-01-11 16:49:13 -0300

    iniciando forms

---
+++ authors/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .forms import RegisterForm
+
+# Create your views here.
+def register_view(request):
+    if request.POST:
+        form = RegisterForm(request.POST)
+    else:
+        form = RegisterForm()
+    form = RegisterForm()
+    return render(request,'authors/pages/register_view.html',{
+        'form': form,
+    })
