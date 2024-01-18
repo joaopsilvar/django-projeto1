fa956204771ca1b1c8f3427e3caa7a001fbf6959
João Paulo <joaopaulosilva39.jp@gmail.com>
2024-01-18 11:44:27 -0300

    test forms

--- authors/forms.py
+++ authors/forms.py
@@ -1,7 +1,9 @@
+import re
+
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-import re
+
 
 def add_attr(field, attr_name, attr_new_val):
     existing = field.widget.attrs.get(attr_name, '')
@@ -11,12 +13,14 @@ def add_attr(field, attr_name, attr_new_val):
 def add_placeholder(field, placeholder_val):
     add_attr(field, 'placeholder', placeholder_val)
 
+
 def strong_password(password):
-    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$')
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
     if not regex.match(password):
         raise ValidationError((
             'Password must have at least one uppercase letter, '
-            'one lowercase letter and one number. The length should be ' 
+            'one lowercase letter and one number. The length should be '
             'at least 8 characters.'
         ),
             code='invalid'
@@ -30,13 +34,12 @@ class RegisterForm(forms.ModelForm):
         add_placeholder(self.fields['email'], 'Your e-mail')
         add_placeholder(self.fields['first_name'], 'Ex.: John')
         add_placeholder(self.fields['last_name'], 'Ex.: Doe')
-        add_attr(self.fields['username'], 'css', 'a-css-class')
+        add_placeholder(self.fields['password'], 'Type your password')
+        add_placeholder(self.fields['password2'], 'Repeat your password')
 
     password = forms.CharField(
         required=True,
-        widget=forms.PasswordInput(attrs={
-            'placeholder': 'Your password'
-        }),
+        widget=forms.PasswordInput(),
         error_messages={
             'required': 'Password must not be empty'
         },
@@ -45,13 +48,13 @@ class RegisterForm(forms.ModelForm):
             'one lowercase letter and one number. The length should be '
             'at least 8 characters.'
         ),
-        validators=[strong_password]
+        validators=[strong_password],
+        label='Password'
     )
     password2 = forms.CharField(
         required=True,
-        widget=forms.PasswordInput(attrs={
-            'placeholder': 'Repeat your password'
-        })
+        widget=forms.PasswordInput(),
+        label='Password2'
     )
 
     class Meta:
@@ -63,13 +66,11 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
-        # exclude = ['first_name']
         labels = {
             'username': 'Username',
             'first_name': 'First name',
             'last_name': 'Last name',
             'email': 'E-mail',
-            'password': 'Password',
         }
         help_texts = {
             'email': 'The e-mail must be valid.',
@@ -79,54 +80,21 @@ class RegisterForm(forms.ModelForm):
                 'required': 'This field must not be empty',
             }
         }
-        widgets = {
-            'first_name': forms.TextInput(attrs={
-                'placeholder': 'Type your username here',
-                'class': 'input text-input'
-            }),
-            'password': forms.PasswordInput(attrs={
-                'placeholder': 'Type your password here'
-            })
-        }
 
-    def clean_password(self):
-        data = self.cleaned_data.get('password')
-
-        if 'atenção' in data:
-            raise ValidationError(
-                'Não digite %(value)s no campo password',
-                code='invalid',
-                params={'value': 'atenção'}
-            )
-
-        return data
-    
-    def clean_first_name(self):
-        data = self.cleaned_data.get('first_name')
-
-        if 'Jonh Doe' in data:
-            raise ValidationError(
-                'Não digite %(value)s no campo First Name',
-                code='invalid',
-                params={'value': 'Jonh Doe'}
-            )
-
-        return data
-    
     def clean(self):
         cleaned_data = super().clean()
+
         password = cleaned_data.get('password')
         password2 = cleaned_data.get('password2')
 
         if password != password2:
             password_confirmation_error = ValidationError(
-                'Password and password must be equal',
+                'Password and password2 must be equal',
                 code='invalid'
             )
-            
             raise ValidationError({
                 'password': password_confirmation_error,
                 'password2': [
-                    password_confirmation_error
-                ]
+                    password_confirmation_error,
+                ],
             })
