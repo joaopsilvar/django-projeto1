1039ddd4c80e08cd37dcfd5b9942abfae44f3988
João Paulo <joaopaulosilva39.jp@gmail.com>
2024-02-07 15:34:31 -0300

    get placelholder selenium

---
+++ tests/functional_tests/authors/base.py
@@ -0,0 +1,19 @@
+import time
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from utils.browser import make_chrome_browser
+from utils.wsl_display import set_display_windows_if_execute_wsl
+
+class AuhtorsBaseTest(StaticLiveServerTestCase):
+    def setUp(self) -> None:
+        #definir display do windows caso estiver executando de um wsl
+        set_display_windows_if_execute_wsl()
+        
+        self.browser = make_chrome_browser()
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+        return super().tearDown()
+
+    def sleep(self, seconds=5):
+        time.sleep(seconds)
