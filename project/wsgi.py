b85ccd6f064bbe629b6ed7e0b4d52b9abbedc25e
JOAO NOT PESSOAL <>
2024-03-06 19:51:05 -0300

    recipe form edit

--- project/wsgi.py
+++ project/wsgi.py
@@ -14,5 +14,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
-from dotenv import load_dotenv
+load_dotenv()
 application = get_wsgi_application()
