bdf70aaf291e2dd7861143b0825ceee546ec77b9
João Paulo <joaopaulosilva39.jp@gmail.com>
2024-03-07 16:39:45 -0300

    Edit

--- authors/views.py
+++ authors/views.py
@@ -111,7 +111,16 @@ def dashboard_recipe_edit(request, id):
         request.POST or None,
         instance=recipe
     )
-    
+    if form.is_valid():
+        recipe = form.save(commit=False)
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+        recipe.save()
+        
+        messages.success(request, 'Sua receita foi salva com sucesso!')
+        return redirect(reverse('authors:dashboard_recipe_edit', args=(id,)))
+        
     return render(
         request,
         'authors/pages/dashboard_recipe.html',
