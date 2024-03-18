025a2d10e3c8fda30fffb2615f4372435cb38582
João Paulo <joaopaulosilva39.jp@gmail.com>
2024-03-18 16:10:36 -0300

    crud compleat

--- authors/views.py
+++ authors/views.py
@@ -181,3 +181,23 @@ def dashboard_recipe_edit(request, id):
             'form': form
         }
     )
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard_recipe_delete(request):
+    if not request.POST:
+        raise Http404()
+    
+    id = request.POST.get('id')
+    
+    recipe = Recipe.objects.filter(
+        is_published=False,
+        author=request.user,
+        pk=id,
+    ).first()
+
+    if not recipe:
+        raise Http404()
+    
+    recipe.delete()
+    messages.success(request, 'Sua receita foi deletada com sucesso!')
+    return redirect(reverse('authors:dashboard'))
