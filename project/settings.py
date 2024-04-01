39f0c75b4a71e0e4af5c636b9705560cf91b91d9
JOAO NOT PESSOAL <>
2024-04-01 19:52:49 -0300

    test

--- project/settings.py
+++ project/settings.py
@@ -83,12 +83,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'basededados',
-        'USER': 'usuario',
-        'PASSWORD': 'senha',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': os.environ.get('DATABASE_ENGINE'),
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
 
